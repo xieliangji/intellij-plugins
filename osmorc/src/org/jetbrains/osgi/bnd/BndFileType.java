eafde669e0d08b67d3c6a8bec76e397f050f8394
xieliangji <450324623@qq.com>
2024-01-09 10:32:02 +0800

    Delete osmorc directory

--- osmorc/src/org/jetbrains/osgi/bnd/BndFileType.java
+++
@@ -1,8 +0,0 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.osgi.bnd;
-
-// [r.sh] will be extended later to a full-blown file type
-public final class BndFileType {
-  public static final String BND_EXT = "bnd";
-  public static final String BND_RUN_EXT = "bndrun";
-}
