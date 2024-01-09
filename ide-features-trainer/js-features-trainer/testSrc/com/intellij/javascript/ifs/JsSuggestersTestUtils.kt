d388479c47ea3db483bf12ed12cf10c8f76a3c4c
xieliangji <450324623@qq.com>
2024-01-09 10:32:57 +0800

    Delete ide-features-trainer directory

--- ide-features-trainer/js-features-trainer/testSrc/com/intellij/javascript/ifs/JsSuggestersTestUtils.kt
+++
@@ -1,5 +0,0 @@
-package com.intellij.javascript.ifs
-
-object JsSuggestersTestUtils {
-  const val relativeTestDataPath = "/contrib/ide-features-trainer/js-features-trainer/testData"
-}
--- ide-features-trainer/ruby-features-trainer/src/org/jetbrains/ruby/ift/lesson/navigation/RubySearchEverywhereLesson.kt
+++
@@ -1,9 +0,0 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.ruby.ift.lesson.navigation
-
-import training.learn.lesson.general.navigation.SearchEverywhereLesson
-
-class RubySearchEverywhereLesson : SearchEverywhereLesson() {
-  override val sampleFilePath = "src/declaration_and_usages_demo.rb"
-  override val resultFileName: String = "quadratic_equations_solver.rb"
-}
